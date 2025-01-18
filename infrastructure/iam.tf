d6734781532a0241dd58f56239cb15e32a19b35d
Tim Gibbon <tng@chegwin.org>
2025-01-18 16:11:45 +0000

    Comment out the sleep which depends on gke

--- infrastructure/iam.tf
+++ infrastructure/iam.tf
@@ -12,16 +12,16 @@ resource "google_project_iam_member" "this" {
 
 # The creation of the binding was failing due to the nodes not being found.
 # This 5 seconds sleeps fixes the issue.
-resource "time_sleep" "wait_5_seconds" {
-  depends_on      = [google_container_cluster.primary]
-  create_duration = "5s"
-}
+#resource "time_sleep" "wait_5_seconds" {
+#  depends_on      = [google_container_cluster.primary]
+#  create_duration = "5s"
+#}
 
-resource "google_service_account_iam_binding" "k8s" {
-  depends_on         = [time_sleep.wait_5_seconds]
-  service_account_id = google_service_account.k8s.name
-  role               = "roles/iam.workloadIdentityUser"
-  members = [
-    "serviceAccount:${var.project}.svc.id.goog[external-secrets/external-secrets]",
-  ]
-}
+#resource "google_service_account_iam_binding" "k8s" {
+#  depends_on         = [time_sleep.wait_5_seconds]
+#  service_account_id = google_service_account.k8s.name
+#  role               = "roles/iam.workloadIdentityUser"
+#  members = [
+#    "serviceAccount:${var.project}.svc.id.goog[external-secrets/external-secrets]",
+#  ]
+#}

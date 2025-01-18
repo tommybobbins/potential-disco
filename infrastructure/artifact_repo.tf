5b1950bcf31a961c71cb35f3e5eb7d3992f2651c
Tim Gibbon <tng@chegwin.org>
2025-01-18 16:31:48 +0000

    Convert list to a set

--- infrastructure/artifact_repo.tf
+++ infrastructure/artifact_repo.tf
@@ -1,5 +1,5 @@
 resource "google_artifact_registry_repository" "repo" {
-  for_each      = var.container_artifact_registry
+  for_each      = toset(var.container_artifact_registry)
   location      = var.region
   repository_id = each.key
   description   = "${each.key} Respository"

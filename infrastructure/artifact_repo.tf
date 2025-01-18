2fb5738481d8552f84c2ff7ed1784c9c69ef6608
Tim Gibbon <tng@chegwin.org>
2025-01-18 16:33:50 +0000

    Unstub the apply

--- infrastructure/artifact_repo.tf
+++ infrastructure/artifact_repo.tf
@@ -2,6 +2,6 @@ resource "google_artifact_registry_repository" "repo" {
   for_each      = toset(var.container_artifact_registry)
   location      = var.region
   repository_id = each.key
-  description   = "${each.key} Respository"
+  description   = "${each.key} Repository"
   format        = "DOCKER"
 }

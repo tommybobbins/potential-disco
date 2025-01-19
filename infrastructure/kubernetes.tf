2109924c7f077abeeb0d742ffd54a7e92241ed13
Tim Gibbon <tng@chegwin.org>
2025-01-19 11:55:30 +0000

    Provider should be less verbose

--- infrastructure/kubernetes.tf
+++ infrastructure/kubernetes.tf
@@ -5,21 +5,21 @@
 # https://learn.hashicorp.com/terraform/kubernetes/provision-gke-cluster#optional-configure-terraform-kubernetes-provider
 # To learn how to schedule deployments and services using the provider, go here: https://learn.hashicorp.com/tutorials/terraform/kubernetes-provider.
 
-provider "kubernetes" {
-  load_config_file = "false"
+#provider "kubernetes" {
+#  load_config_file = "false"
+#
+#  host     = google_container_cluster.primary.endpoint
+#  client_certificate     = google_container_cluster.primary.master_auth.0.client_certificate
+#  client_key             = google_container_cluster.primary.master_auth.0.client_key
+#  cluster_ca_certificate = google_container_cluster.primary.master_auth.0.cluster_ca_certificate
+#}
 
-  host     = google_container_cluster.primary.endpoint
-  client_certificate     = google_container_cluster.primary.master_auth.0.client_certificate
-  client_key             = google_container_cluster.primary.master_auth.0.client_key
-  cluster_ca_certificate = google_container_cluster.primary.master_auth.0.cluster_ca_certificate
-}
 
-
-provider "helm" {
-    kubernetes {
-        host     = google_container_cluster.primary.endpoint
-        client_certificate     = google_container_cluster.primary.master_auth.0.client_certificate
-        client_key             = google_container_cluster.primary.master_auth.0.client_key
-        cluster_ca_certificate = google_container_cluster.primary.master_auth.0.cluster_ca_certificate
-    }
-}
+#provider "helm" {
+#    kubernetes {
+#        host     = google_container_cluster.primary.endpoint
+#        client_certificate     = google_container_cluster.primary.master_auth.0.client_certificate
+#        client_key             = google_container_cluster.primary.master_auth.0.client_key
+#        cluster_ca_certificate = google_container_cluster.primary.master_auth.0.cluster_ca_certificate
+#    }
+#}

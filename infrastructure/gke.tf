6beb845c255fcc8c724289ba7e4e198468a8c0db
Tim Gibbon <tng@chegwin.org>
2025-01-19 17:21:57 +0000

    Monitoring for only pod

--- infrastructure/gke.tf
+++ infrastructure/gke.tf
@@ -38,7 +38,7 @@ resource "google_container_cluster" "primary" {
   deletion_protection = false
 
   monitoring_config {
-  enable_components = []
+  enable_components = ["POD"]
   managed_prometheus {
     enabled = true
   }

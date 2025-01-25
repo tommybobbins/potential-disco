cc12a515843ad22d3c5f79b8c285c2bc3b9656ea
Tim Gibbon <tng@chegwin.org>
2025-01-25 12:23:05 +0000

    Add argo_manifest.tf

--- infrastructure/argo_manifest.tf
+++ infrastructure/argo_manifest.tf
@@ -1,14 +1,14 @@
-#resource "kubectl_manifest" "argo_application" {
-#  for_each = toset(var.argocd_applications)
-#  yaml_body = templatefile("../helm/argo_applications/application.yaml",
-#    {
-#      NAME            = "${each.value}"
-#      GH_URL          = "foobar"
-#      ENV             = "main"
-#      PATH            = each.value
-#      NAMESPACE       = "scrapers"
-#  })
-#  depends_on = [
-#    helm_release.argocd,
-#  ]
-#}
+resource "kubectl_manifest" "argo_application" {
+  for_each = toset(var.argocd_applications)
+  yaml_body = templatefile("../helm/argo_applications/application.yaml",
+    {
+      NAME            = "${each.value}"
+      GH_URL          = "https://github.com/tommybobbins/potential-disco"
+      ENV             = "main"
+      PATH            = "helm/argo_applications/${each.value}"
+      NAMESPACE       = "scrapers"
+  })
+  depends_on = [
+    helm_release.argocd,
+  ]
+}

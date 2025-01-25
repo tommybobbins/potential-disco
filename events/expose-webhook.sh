4927524fce92cd0c202fca398c59e2e51d453973
Tim Gibbon <tng@chegwin.org>
2025-01-25 16:19:22 +0000

    Do not need events

--- events/expose-webhook.sh
+++
@@ -1,2 +0,0 @@
-kubectl port-forward -n argo-events  $(kubectl -n argo-events get pod -l eventsource-name=webhook -o name) 12000:12000
-#kubectl port-forward -n argo-events  $(kubectl -n argo-events get pod -l app=events-webhook -o name) 12000:12000
--- events/runcurl.sh
+++
@@ -1,1 +0,0 @@
-curl -d '{"message":"this is my first webhook"}' -H "Content-Type: application/json" -X POST https://localhost:12000/example -k --http1.1

522531fecf3312ce096e9260c74f2249a51781bd
Tim Gibbon <tng@chegwin.org>
2026-05-13 11:41:43 +0100

    Add dashboards for Trivy

--- infrastructure/cloud_observability.tf
+++ infrastructure/cloud_observability.tf
@@ -1,3 +1,15 @@
-resource "google_monitoring_dashboard" "dashboard" {
+resource "google_monitoring_dashboard" "Carbon_Intensity_Generation_Mix" {
   dashboard_json = file("./json/Carbon_Intensity_Generation_Mix.json")
+}
+
+resource "google_monitoring_dashboard" "trivy_operator" {
+  dashboard_json = file("./json/trivy_operator_dash.json")
+}
+
+resource "google_monitoring_dashboard" "trivy_operator_vuln" {
+  dashboard_json = file("./json/trivy_operator_vuln.json")
+}
+
+resource "google_monitoring_dashboard" "trivy_workload_vuln" {
+  dashboard_json = file("./json/trivy_workload_vuln.json")
 }

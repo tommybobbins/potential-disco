be5998feb6dfd8609075365f3137f7bdba5af7d1
Tim Gibbon <tng@chegwin.org>
2026-05-14 15:24:52 +0100

    Remove auto monitoring config

--- infrastructure/gke.tf
+++ infrastructure/gke.tf
@@ -41,9 +41,6 @@ resource "google_container_cluster" "primary" {
     enable_components = ["SYSTEM_COMPONENTS", "POD"]
     managed_prometheus {
       enabled = true
-        auto_monitoring_config {
-          scope = "ALL"
-        }
     }
   }
 
